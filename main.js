549a8519f38d64ac5798ae3ceb9c7d4c6c827620
38amit <75780317+38amit@users.noreply.github.com>
2021-08-03 17:08:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload() {
+}
+
+function setup() {
+canvas = createCanvas(640, 480);
+canvas.position(290, 310);
+video = createCapture(VIDEO);
+video.hide();
+tint_color = "";
+}
+
+function draw() {
+    image(video, 0, 0, 640, 480);
+    tint(tint_color);
+}
+
+function take_snapshot() {
+    save("tinted_image.png");
+}
+
+function filter_tint() {
+    tint_color = document.getElementById("filter_name").value;
+}
